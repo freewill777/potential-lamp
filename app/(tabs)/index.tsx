fe8b95f5886719ce894596582ea9e2c21d06115f
Cristian Sfetcu <sfetcu.cristian93@gmail.com>
2023-09-19 04:41:20 +0300

    floating action button UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,6 @@ import { Posts, fetchPosts } from "../../src/lib/api";
 import { supabase } from "../../src/lib/supabase";
 import PostCard from "../../src/components/PostCard";
 import { useUserInfo } from "../../src/lib/userContext";
-import Stories from "../../src/subviews/Stories";
 
 export default function TabOneScreen() {
   const [posts, setPosts] = useState<Posts>([]);
--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -1,25 +1,88 @@
 import { Feather, MaterialIcons } from "@expo/vector-icons";
-import { TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+import { TouchableOpacity, StyleSheet, Dimensions, View } from "react-native";
 import Colors from "../../enums";
 import { useState } from "react";
-
+import { Text } from "./Themed";
 const { height } = Dimensions.get("window");
 
-const FloatingActionButtonSingle = ({ style, onPress, Icon }: any) => {
+const FloatingActionButtonSingle = ({ style, onPress, Icon, text }: any) => {
   return (
-    <TouchableOpacity
-      style={[styles.fab, { backgroundColor: Colors.TurquoiseDark }, style]}
-      onPress={onPress}
-    >
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <Text style={{ color: "white", marginRight: 10 }}>{text}</Text>
+      <TouchableOpacity
+        style={[styles.fab, { backgroundColor: Colors.TurquoiseDark }, style]}
+        onPress={onPress}
+      >
+        {Icon ? (
+          <Icon />
+        ) : (
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text style={{ color: Colors.BlackBlue, marginRight: 10 }}>
+              {text}
+            </Text>
+            <View
+              style={{
+                borderColor: Colors.GrayBeige,
+                backgroundColor: Colors.TurquoiseDark,
+                borderRadius: 50,
+                padding: 25,
+              }}
+            >
+              <MaterialIcons name="add" size={25} color={Colors.GrayBeige} />
+            </View>
+          </View>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const FloatingActionButtonSingleRoot = ({
+  style,
+  onPress,
+  Icon,
+  text,
+}: any) => {
+  return (
+    <TouchableOpacity style={[styles.fab, style]} onPress={onPress}>
       {Icon ? (
-        <Icon />
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text
+            style={{
+              color: Colors.BlackBlue,
+              marginRight: 10,
+              fontWeight: "900",
+            }}
+          >
+            {text}
+          </Text>
+          <View
+            style={{
+              borderColor: Colors.GrayBeige,
+              backgroundColor: Colors.TurquoiseDark,
+              borderRadius: 50,
+              padding: 15,
+            }}
+          >
+            <Icon />
+          </View>
+        </View>
       ) : (
-        <MaterialIcons
-          name="add"
-          size={25}
-          color={Colors.GrayBeige}
-          style={{ borderColor: Colors.GrayBeige }}
-        />
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={{ color: Colors.BlackBlue, marginRight: 10 }}>
+            {text}
+          </Text>
+          <View
+            style={{
+              borderColor: Colors.GrayBeige,
+              backgroundColor: Colors.TurquoiseDark,
+              borderRadius: 50,
+              padding: 18,
+            }}
+          >
+            <MaterialIcons name="add" size={25} color={Colors.GrayBeige} />
+          </View>
+        </View>
       )}
     </TouchableOpacity>
   );
@@ -30,30 +93,40 @@ const FloatingActionButton = () => {
 
   return (
     <>
-      <FloatingActionButtonSingle
-        onPress={() => setShowMoreAddOptions(!showMoreAddOptions)}
-      />
+      {!showMoreAddOptions && (
+        <FloatingActionButtonSingleRoot
+          onPress={() => setShowMoreAddOptions(!showMoreAddOptions)}
+          text=""
+        />
+      )}
       {showMoreAddOptions && (
-        <>
-          <FloatingActionButtonSingle
-            style={{ marginVertical: height / 7 + 60 }}
+        <View style={styles.overlay}>
+          <FloatingActionButtonSingleRoot
+            onPress={() => setShowMoreAddOptions(!showMoreAddOptions)}
+            text=""
+          />
+          <FloatingActionButtonSingleRoot
+            style={{ marginVertical: height / 7 + 80, paddingRight: 7 }}
             Icon={() => (
               <Feather name="file" size={18} color={Colors.GrayBeige} />
             )}
+            text="Create new story"
           />
-          <FloatingActionButtonSingle
-            style={{ marginVertical: height / 7 + 120 }}
+          <FloatingActionButtonSingleRoot
+            style={{ marginVertical: height / 7 + 140, paddingRight: 7 }}
             Icon={() => (
               <Feather name="camera" size={18} color={Colors.GrayBeige} />
             )}
+            text="Create new post"
           />
-          <FloatingActionButtonSingle
-            style={{ marginVertical: height / 7 + 180 }}
+          <FloatingActionButtonSingleRoot
+            style={{ marginVertical: height / 7 + 200, paddingRight: 7 }}
             Icon={() => (
               <Feather name="tv" size={18} color={Colors.GrayBeige} />
             )}
+            text="Create new reel"
           />
-        </>
+        </View>
       )}
     </>
   );
@@ -68,10 +141,15 @@ const styles = StyleSheet.create({
     marginHorizontal: 30,
     right: 0,
     bottom: 0,
-    borderRadius: 30,
-    height: 50,
-    width: 50,
     alignItems: "center",
     justifyContent: "center",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
   },
 });
