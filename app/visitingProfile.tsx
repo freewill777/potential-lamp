d60ce47922e112d538b3034c23d16b2763095564
Cristian Sfetcu <sfetcu.cristian93@gmail.com>
2023-12-06 02:19:12 +0200

    fix

--- app/visitingProfile.tsx
+++ app/visitingProfile.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, View, Text } from "react-native";
 import { supabase } from "../src/lib/supabase";
-import { PostCard, FriendSystem, UserProfile } from "../src/components";
+import { PostCard, FriendSystem } from "../src/components";
 import { useSearchParams } from "expo-router";
 import { Stack } from "expo-router";
 import { Post, User } from "../src/lib/api";
+import UserPostsView from "../src/components/UserPostsView";
+
 const VisitingProfile = () => {
   const { userId } = useSearchParams();
   const [posts, setPosts] = useState<Post[]>([]);
-const [userProfile, setUserProfile] = useState<User | null>(null);
-useEffect(() => {
+  const [userProfile, setUserProfile] = useState<User | null>(null);
+
+  useEffect(() => {
     fetchPosts();
-fetchUserProfile();
-}, []);
+    fetchUserProfile();
+  }, []);
 
   const fetchPosts = async () => {
     const { data, error } = await supabase
@@ -22,20 +25,18 @@ fetchUserProfile();
     if (error) console.log("Error fetching posts: ", error);
     else setPosts(data);
   };
-const fetchUserProfile = async () => {
+  const fetchUserProfile = async () => {
     const { data, error } = await supabase
       .from("profiles")
-      .select("*")
+      .select("*, profile: profiles(username, avatar_url)")
       .eq("id", userId)
       .single();
     if (error) console.log("Error fetching user profile: ", error);
     else setUserProfile(data);
   };
-return (
+  return (
     <View>
-<Stack.Screen options={{ title: userProfile?.username || "" }} />
-      {userProfile && <UserProfile profile={userProfile} />}
-{typeof userId === "string" && <FriendSystem userId={userId} />}
+      <Stack.Screen options={{ title: userProfile?.username || "" }} />
       <FlatList
         data={posts}
         keyExtractor={(item) => item.id}
