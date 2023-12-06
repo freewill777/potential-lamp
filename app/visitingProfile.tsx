5821f7f80520a79120b87d8f39615040a68e88c8
DevGPT-AI <sfetcu.cristian93@gmail.com>
2023-12-06 00:00:36 +0000

    Refactor app/visitingProfile.tsx to align with app/(tabs)/profile.tsx structure while maintaining the functionality to view other users' profiles.

--- app/visitingProfile.tsx
+++ app/visitingProfile.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, View, Text } from "react-native";
 import { supabase } from "../src/lib/supabase";
-import { PostCard, FriendSystem } from "../src/components";
+import { PostCard, FriendSystem, UserProfile } from "../src/components";
 import { useSearchParams } from "expo-router";
 import { Stack } from "expo-router";
-import { Post } from "../src/lib/api";
-
+import { Post, User } from "../src/lib/api";
 const VisitingProfile = () => {
   const { userId } = useSearchParams();
   const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
+const [userProfile, setUserProfile] = useState<User | null>(null);
+useEffect(() => {
     fetchPosts();
-  }, []);
+fetchUserProfile();
+}, []);
 
   const fetchPosts = async () => {
     const { data, error } = await supabase
@@ -22,11 +22,20 @@ const VisitingProfile = () => {
     if (error) console.log("Error fetching posts: ", error);
     else setPosts(data);
   };
-
-  return (
+const fetchUserProfile = async () => {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", userId)
+      .single();
+    if (error) console.log("Error fetching user profile: ", error);
+    else setUserProfile(data);
+  };
+return (
     <View>
-      <Stack.Screen options={{ title: "" }} />
-      {typeof userId === "string" && <FriendSystem userId={userId} />}
+<Stack.Screen options={{ title: userProfile?.username || "" }} />
+      {userProfile && <UserProfile profile={userProfile} />}
+{typeof userId === "string" && <FriendSystem userId={userId} />}
       <FlatList
         data={posts}
         keyExtractor={(item) => item.id}
